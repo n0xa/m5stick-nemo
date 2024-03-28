4c3ba18bbfe2b683cd11e92c3e086f11c021e830
danny8972 <150498832+danny8972@users.noreply.github.com>
2024-03-27 21:51:19 -0700

    Made Random ESP MAC address a function to be called later

--- m5stick-nemo.ino
+++ m5stick-nemo.ino
@@ -1425,6 +1425,21 @@ void btmenu_loop() {
   }
 }
 
+void random_MAC(){
+  uint8_t tempMAC[ESP_BD_ADDR_LEN];
+      
+  // Keep ESP Manufactorer ID 
+  tempMAC[0] = 0x02;
+  tempMAC[1] = 0xE5;
+
+  // Generate random values for the MAC address and add them to the ESP ID
+  uint32_t randomBytes = esp_random();
+  memcpy(&tempMAC[2], &randomBytes, 4);
+
+  // Set new MAC address
+  esp_base_mac_addr_set(tempMAC);
+}
+
 MENU ajmenu[] = {
   { TXT_BACK, 30},
   { "AirPods", 1},
@@ -1487,19 +1502,7 @@ void aj_loop(){
   if (check_select_press() || maelstrom) {
     deviceType = ajmenu[cursor].command;
     if (maelstrom) {
-      uint8_t newMAC[ESP_BD_ADDR_LEN];
-      
-      // Keep ESP Manufactorer ID 
-      newMAC[0] = 0x02;
-      newMAC[1] = 0xE5;
-
-      // Generate random values for the MAC address and add them to the ESP ID
-      uint32_t randomBytes = esp_random();
-      memcpy(&newMAC[2], &randomBytes, 4);
-
-      // Set new MAC address
-      esp_base_mac_addr_set(newMAC);
-
+      random_MAC();
       deviceType = random(1, 28);
     }
     switch(deviceType) {
